d61089d3e380e92700a3d09f47e71d198522063b
Kapil bavisiya <kapilbavisiya555@gmail.com>
2020-12-09 18:47:24 +0530

    gin-gonic

---
+++ src/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Kapil B Bavisiya",
+	})
+}
+
+func PostHomePage(c *gin.Context) {
+	body := c.Request.Body
+	value, err := ioutil.ReadAll(body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, gin.H{
+		"message": string(value),
+	})
+}
+
+func QueryStrings(c *gin.Context) {
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
+func PathParameters(c *gin.Context) {
+	name := c.Param("name")
+	age := c.Param("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	r := gin.Default()
+	r.GET("/", HomePage)
+	r.POST("/", PostHomePage)
+	r.GET("/query", QueryStrings)             // /query?name=kapil&age=21
+	r.GET("/path/:name/:age", PathParameters) // /path/kapil/21
+	r.Run()
+
+}
